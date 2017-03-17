076b1a494232294b75e500fcd753f516dc341eea
Matthew Riddell <matthewriddell@Matthews-iMac.att.net>
2017-03-17 00:55:55 -0700

    Beginning Project: README text and main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//main.cpp
+using namespace std;
+
+int int main(int argc, char const *argv[]) {
+  //argv[0] - Program Name
+  //argv[1] - Command File
+  //argv[2] - Training File
+  //argv[3] - Output File
+  return 0;
+}
